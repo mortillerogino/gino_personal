ad1605ca0030ff055a11e322ef531f858ad3e05f
mortillerogino <mortillerogino@gmail.com>
2019-05-07 21:30:44 +0800

    Fixed mapping to ignore id only for dto to domain mapping. Added MembershipTypeDto with mapping. Included membershiptypedto on customers get

--- Vidly/Controllers/Api/CustomersController.cs
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -23,7 +23,10 @@ namespace Vidly.Controllers.Api
         // GET /api/customers
         public IHttpActionResult GetCustomers()
         {
-            var customerDtos = _context.Customers.ToList().Select(Mapper.Map<Customer, CustomerDto>);
+            var customerDtos = _context.Customers
+                .Include(c => c.MembershipType)
+                .ToList()
+                .Select(Mapper.Map<Customer, CustomerDto>);
 
             return Ok(customerDtos);
         }
